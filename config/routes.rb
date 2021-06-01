a2f6599bca18c7c04fc2618d146c89615ea85f7f
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-01 12:51:08 +0200

    Ajout des controllers et views correspondantes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'landing_page#index'
+  resources 'landing_page', only: [:index]
+  resources 'contacts', only: [:index]
+  resources 'about', only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
